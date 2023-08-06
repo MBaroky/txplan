498bdc20473506cc0ad4951b5f20ebd9529ea103
MBaroky <m.baroky@gmail.com>
2023-08-06 12:06:59 +0200

    reversed prev commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,16 +75,13 @@
     }
 
     if (!hasTouchScreen) {
-      setTimeout(() => {
-        target.querySelectorAll(".mouse").forEach(shift => {
-          const position = shift.getAttribute("value");
-          const x = (window.innerWidth - event.pageX * position) / 90;
-          const y =
-            (window.innerHeight - event.pageY * position) / 90;
+      target.querySelectorAll(".mouse").forEach(shift => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
 
-          shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
-        });
-      }, 200);
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      });
     }
   }
 })(); // End of use strict
