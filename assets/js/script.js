df91031d362a97dcfab6a8707439d2f42ba7b187
MBaroky <m.baroky@gmail.com>
2023-08-06 12:05:30 +0200

    minor fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,13 +75,16 @@
     }
 
     if (!hasTouchScreen) {
-      target.querySelectorAll(".mouse").forEach(shift => {
-        const position = shift.getAttribute("value");
-        const x = (window.innerWidth - event.pageX * position) / 90;
-        const y = (window.innerHeight - event.pageY * position) / 90;
+      setTimeout(() => {
+        target.querySelectorAll(".mouse").forEach(shift => {
+          const position = shift.getAttribute("value");
+          const x = (window.innerWidth - event.pageX * position) / 90;
+          const y =
+            (window.innerHeight - event.pageY * position) / 90;
 
-        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
-      });
+          shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+      }, 200);
     }
   }
 })(); // End of use strict
